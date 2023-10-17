f916ce63eef0d9c14333a1904755b710b069b8af
Georg Schramm <georg.schramm@kuleuven.be>
2023-10-17 18:17:20 +0200

    move custom layers to separate function

--- 01_torch_projection_layer.py
+++ 01_torch_projection_layer.py
@@ -2,132 +2,10 @@ from __future__ import annotations
 
 import utils
 import parallelproj
-import array_api_compat.numpy as np
 import array_api_compat.torch as torch
-import matplotlib.pyplot as plt
-from array_api_compat import device, to_device
+from array_api_compat import device
 
-class LinearSingleChannel3DOperator(torch.autograd.Function):
-    """
-    We can implement our own custom autograd Functions by subclassing
-    torch.autograd.Function and implementing the forward and backward passes
-    which operate on Tensors.
-    """
-
-    @staticmethod
-    def forward(ctx, x, operator: parallelproj.LinearOperator):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation.
-        """
-       
-        ctx.set_materialize_grads(False)
-        ctx.operator = operator
-
-        batch_size = x.shape[0]
-        y = torch.zeros((batch_size,1) + operator.out_shape, dtype = x.dtype, 
-                        device = device(x))
-
-
-        # loop over all samples in the batch and apply linear operator
-        # to the first channel
-        for i in range(batch_size):
-            y[i,0,:,:,:] = operator(x[i,0,:,:,:].detach())
-
-        return y
-
-    @staticmethod
-    def backward(ctx, grad_output: torch.Tensor):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-
-        For details on how to implement the backward pass, see
-        https://pytorch.org/docs/stable/notes/extending.html#how-to-use
-        """
-
-        # since forward takes two input arguments (x, operator)
-        # we have to return two arguments (the latter is None)
-        if grad_output is None:
-            return None, None
-        else:
-            operator = ctx.operator
-
-            batch_size = grad_output.shape[0]
-            x = torch.zeros((batch_size,1) + operator.in_shape, dtype = grad_output.dtype, 
-                            device = device(grad_output))
-            
-            # loop over all samples in the batch and apply linear operator
-            # to the first channel
-            for i in range(batch_size):
-                x[i,0,:,:,:] = operator.adjoint(grad_output[i,0,:,:,:].detach())
-
-            return x, None
-
-
-class AdjointLinearSingleChannel3DOperator(torch.autograd.Function):
-    """
-    We can implement our own custom autograd Functions by subclassing
-    torch.autograd.Function and implementing the forward and backward passes
-    which operate on Tensors.
-    """
-
-    @staticmethod
-    def forward(ctx, x, operator: parallelproj.LinearOperator):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation.
-        """
-
-        ctx.set_materialize_grads(False)
-        ctx.operator = operator
-
-        batch_size = x.shape[0]
-        y = torch.zeros((batch_size,1) + operator.in_shape, dtype = x.dtype, 
-                        device = device(x))
-
-
-        # loop over all samples in the batch and apply linear operator
-        # to the first channel
-        for i in range(batch_size):
-            y[i,0,:,:,:] = operator.adjoint(x[i,0,:,:,:].detach())
-
-        return y
-
-
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-
-        For details on how to implement the backward pass, see
-        https://pytorch.org/docs/stable/notes/extending.html#how-to-use
-        """
-
-        # since forward takes two input arguments (x, operator)
-        # we have to return two arguments (the latter is None)
-        if grad_output is None:
-            return None, None
-        else:
-            operator = ctx.operator
-
-            batch_size = grad_output.shape[0]
-            x = torch.zeros((batch_size,1) + operator.out_shape, dtype = grad_output.dtype, 
-                            device = device(grad_output))
-            
-            # loop over all samples in the batch and apply linear operator
-            # to the first channel
-            for i in range(batch_size):
-                x[i,0,:,:,:] = operator(grad_output[i,0,:,:,:].detach())
-
-            return x, None
-
+from layers import LinearSingleChannel3DOperator, AdjointLinearSingleChannel3DOperator
 
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
@@ -169,12 +47,14 @@ projector = utils.RegularPolygonPETNonTOFProjector(lor_descriptor, img_shape,
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 
-x = torch.rand((1,1) + projector.in_shape,
+batch_size = 2
+
+x = torch.rand((batch_size,1) + projector.in_shape,
                device=dev,
                dtype=torch.float32,
                requires_grad=True)
 
-y = torch.rand((1,1) + projector.out_shape,
+y = torch.rand((batch_size,1) + projector.out_shape,
                device=dev,
                dtype=torch.float32,
                requires_grad=True)
@@ -207,14 +87,18 @@ dummy_loss.backward()
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 
-print('Running forward projection layer gradient test')
-grad_test_fwd = torch.autograd.gradcheck(fwd_op_layer, (x, projector),
-                                         eps=1e-1,
-                                         atol=1e-4,
-                                         rtol=1e-4)
-
-print('Running adjoint projection layer gradient test')
-grad_test_fwd = torch.autograd.gradcheck(adjoint_op_layer, (y, projector),
-                                         eps=1e-1,
-                                         atol=1e-4,
-                                         rtol=1e-4)
+if dev == 'cpu':
+    print('skipping (slow) gradient checks on cpu')
+else:
+    print('Running forward projection layer gradient test')
+    grad_test_fwd = torch.autograd.gradcheck(fwd_op_layer, (x, projector),
+                                             eps=1e-1,
+                                             atol=1e-4,
+                                             rtol=1e-4)
+    
+    print('Running adjoint projection layer gradient test')
+    grad_test_fwd = torch.autograd.gradcheck(adjoint_op_layer, (y, projector),
+                                             eps=1e-1,
+                                             atol=1e-4,
+                                             rtol=1e-4)
+    
---
+++ layers.py
@@ -0,0 +1,164 @@
+from __future__ import annotations
+
+import torch
+import parallelproj
+from array_api_compat import device
+
+# see also: https://pytorch.org/tutorials/beginner/examples_autograd/two_layer_net_custom_function.html
+
+class LinearSingleChannel3DOperator(torch.autograd.Function):
+    """
+    Function representing a linear operator acting on a mini batch of single channel 3D images
+    """
+
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, operator: parallelproj.LinearOperator) -> torch.Tensor:
+        """forward pass of the linear operator
+
+        Parameters
+        ----------
+        ctx : context object
+            that can be used to store information for the backward pass 
+        x : torch.Tensor
+            mini batch of 3D images with dimension (batch_size, 1, num_voxels_x, num_voxels_y, num_voxels_z)
+        operator : parallelproj.LinearOperator
+            linear operator that can act on a single 3D image
+
+        Returns
+        -------
+        torch.Tensor
+            mini batch of 3D images with dimension (batch_size, 1, opertor.out_shape)
+        """
+       
+        ctx.set_materialize_grads(False)
+        ctx.operator = operator
+
+        batch_size = x.shape[0]
+        y = torch.zeros((batch_size,1) + operator.out_shape, dtype = x.dtype, 
+                        device = device(x))
+
+
+        # loop over all samples in the batch and apply linear operator
+        # to the first channel
+        for i in range(batch_size):
+            y[i,0,:,:,:] = operator(x[i,0,:,:,:].detach())
+
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> tuple[torch.Tensor, None]:
+        """backward pass of the forward pass
+
+        Parameters
+        ----------
+        ctx : context object
+            that can be used to obtain information from the forward pass
+        grad_output : torch.Tensor
+            mini batch of dimension (batch_size, 1, operator.out_shape)
+
+        Returns
+        -------
+        torch.Tensor, None
+            mini batch of 3D images with dimension (batch_size, 1, opertor.in_shape)
+        """
+ 
+        #For details on how to implement the backward pass, see
+        #https://pytorch.org/docs/stable/notes/extending.html#how-to-use
+
+        # since forward takes two input arguments (x, operator)
+        # we have to return two arguments (the latter is None)
+        if grad_output is None:
+            return None, None
+        else:
+            operator = ctx.operator
+
+            batch_size = grad_output.shape[0]
+            x = torch.zeros((batch_size,1) + operator.in_shape, dtype = grad_output.dtype, 
+                            device = device(grad_output))
+            
+            # loop over all samples in the batch and apply linear operator
+            # to the first channel
+            for i in range(batch_size):
+                x[i,0,:,:,:] = operator.adjoint(grad_output[i,0,:,:,:].detach())
+
+            return x, None
+
+
+class AdjointLinearSingleChannel3DOperator(torch.autograd.Function):
+    """
+    Function representing the adjoint of a linear operator acting on a mini batch of single channel 3D images
+    """
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, operator: parallelproj.LinearOperator) -> torch.Tensor:
+        """forward pass of the adjoint of the linear operator
+
+        Parameters
+        ----------
+        ctx : context object
+            that can be used to store information for the backward pass 
+        x : torch.Tensor
+            mini batch of 3D images with dimension (batch_size, 1, operator.out_shape)
+        operator : parallelproj.LinearOperator
+            linear operator that can act on a single 3D image
+
+        Returns
+        -------
+        torch.Tensor
+            mini batch of 3D images with dimension (batch_size, 1, opertor.in_shape)
+        """
+ 
+        ctx.set_materialize_grads(False)
+        ctx.operator = operator
+
+        batch_size = x.shape[0]
+        y = torch.zeros((batch_size,1) + operator.in_shape, dtype = x.dtype, 
+                        device = device(x))
+
+
+        # loop over all samples in the batch and apply linear operator
+        # to the first channel
+        for i in range(batch_size):
+            y[i,0,:,:,:] = operator.adjoint(x[i,0,:,:,:].detach())
+
+        return y
+
+
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """backward pass of the forward pass
+
+        Parameters
+        ----------
+        ctx : context object
+            that can be used to obtain information from the forward pass
+        grad_output : torch.Tensor
+            mini batch of dimension (batch_size, 1, operator.in_shape)
+
+        Returns
+        -------
+        torch.Tensor, None
+            mini batch of 3D images with dimension (batch_size, 1, opertor.out_shape)
+        """
+ 
+        #For details on how to implement the backward pass, see
+        #https://pytorch.org/docs/stable/notes/extending.html#how-to-use
+
+        # since forward takes two input arguments (x, operator)
+        # we have to return two arguments (the latter is None)
+        if grad_output is None:
+            return None, None
+        else:
+            operator = ctx.operator
+
+            batch_size = grad_output.shape[0]
+            x = torch.zeros((batch_size,1) + operator.out_shape, dtype = grad_output.dtype, 
+                            device = device(grad_output))
+            
+            # loop over all samples in the batch and apply linear operator
+            # to the first channel
+            for i in range(batch_size):
+                x[i,0,:,:,:] = operator(grad_output[i,0,:,:,:].detach())
+
+            return x, None
+
