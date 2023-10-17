b79b4cd6256ceb07df69fe322ecfaef539c86f11
Georg Schramm <georg.schramm@kuleuven.be>
2023-10-17 12:21:03 +0200

    add batch/channel dimension

--- 01_torch_projection_layer.py
+++ 01_torch_projection_layer.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-#TODO: batch + channel dim
 import utils
 import parallelproj
 import array_api_compat.numpy as np
@@ -8,30 +7,8 @@ import array_api_compat.torch as torch
 import matplotlib.pyplot as plt
 from array_api_compat import device, to_device
 
-
-class LinearDummyOperator():
-
-    def __init__(self):
-        self._A = torch.rand(4, 3)
-
-    @property
-    def in_shape(self):
-        return (3, )
-
-    @property
-    def out_shape(self):
-        return (4, )
-
-    def __call__(self, x):
-        return self._A @ x
-
-    def adjoint(self, y):
-        return self._A.T @ y
-
-
-class LinearOperatorForwardLayer(torch.autograd.Function):
-    """PET forward projection layer mapping a 3D image to a 4D sinogram
-
+class Linear3DOperatorForwardLayer(torch.autograd.Function):
+    """
     We can implement our own custom autograd Functions by subclassing
     torch.autograd.Function and implementing the forward and backward passes
     which operate on Tensors.
@@ -44,11 +21,21 @@ class LinearOperatorForwardLayer(torch.autograd.Function):
         a Tensor containing the output. ctx is a context object that can be used
         to stash information for backward computation.
         """
-
+       
         ctx.set_materialize_grads(False)
         ctx.operator = operator
 
-        return operator(x)
+        batch_size = x.shape[0]
+        y = torch.zeros((batch_size,1) + operator.out_shape, dtype = x.dtype, 
+                        device = device(x))
+
+
+        # loop over all samples in the batch and apply linear operator
+        # to the first channel
+        for i in range(batch_size):
+            y[i,0,:,:,:] = operator(x[i,0,:,:,:].detach())
+
+        return y
 
     @staticmethod
     def backward(ctx, grad_output: torch.Tensor):
@@ -61,18 +48,27 @@ class LinearOperatorForwardLayer(torch.autograd.Function):
         https://pytorch.org/docs/stable/notes/extending.html#how-to-use
         """
 
+        # since forward takes two input arguments (x, operator)
+        # we have to return two arguments (the latter is None)
         if grad_output is None:
             return None, None
         else:
             operator = ctx.operator
-            # since forward takes two input arguments (x, operator)
-            # we have to return two arguments (the latter is None)
-            return operator.adjoint(grad_output), None
+
+            batch_size = grad_output.shape[0]
+            x = torch.zeros((batch_size,1) + operator.in_shape, dtype = grad_output.dtype, 
+                            device = device(grad_output))
+            
+            # loop over all samples in the batch and apply linear operator
+            # to the first channel
+            for i in range(batch_size):
+                x[i,0,:,:,:] = operator.adjoint(grad_output[i,0,:,:,:].detach())
+
+            return x, None
 
 
-class LinearOperatorAdjointLayer(torch.autograd.Function):
-    """ adjoint of PET forward projection layer mapping a 4D sinogram to a 3D image
-    
+class Linear3DOperatorAdjointLayer(torch.autograd.Function):
+    """
     We can implement our own custom autograd Functions by subclassing
     torch.autograd.Function and implementing the forward and backward passes
     which operate on Tensors.
@@ -89,7 +85,19 @@ class LinearOperatorAdjointLayer(torch.autograd.Function):
         ctx.set_materialize_grads(False)
         ctx.operator = operator
 
-        return operator.adjoint(x)
+        batch_size = x.shape[0]
+        y = torch.zeros((batch_size,1) + operator.in_shape, dtype = x.dtype, 
+                        device = device(x))
+
+
+        # loop over all samples in the batch and apply linear operator
+        # to the first channel
+        for i in range(batch_size):
+            y[i,0,:,:,:] = operator.adjoint(x[i,0,:,:,:].detach())
+
+        return y
+
+
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -102,11 +110,23 @@ class LinearOperatorAdjointLayer(torch.autograd.Function):
         https://pytorch.org/docs/stable/notes/extending.html#how-to-use
         """
 
+        # since forward takes two input arguments (x, operator)
+        # we have to return two arguments (the latter is None)
         if grad_output is None:
             return None, None
         else:
             operator = ctx.operator
-            return operator(grad_output), None
+
+            batch_size = grad_output.shape[0]
+            x = torch.zeros((batch_size,1) + operator.out_shape, dtype = grad_output.dtype, 
+                            device = device(grad_output))
+            
+            # loop over all samples in the batch and apply linear operator
+            # to the first channel
+            for i in range(batch_size):
+                x[i,0,:,:,:] = operator(grad_output[i,0,:,:,:].detach())
+
+            return x, None
 
 
 #----------------------------------------------------------------------------
@@ -134,61 +154,67 @@ else:
 lor_descriptor = utils.DemoPETScannerLORDescriptor(torch,
                                                    dev,
                                                    num_rings=2,
-                                                   radial_trim=171)
+                                                   radial_trim=201)
 
 # image properties
 voxel_size = (2.66, 2.66, 2.66)
 img_shape = (10, 10, 2 * lor_descriptor.scanner.num_modules)
 
 projector = utils.RegularPolygonPETNonTOFProjector(lor_descriptor, img_shape,
-                                                   voxel_size)
+                                                   voxel_size, 
+                                                   views = torch.arange(0, lor_descriptor.num_views, 34, device = dev))
 
-#projector = LinearDummyOperator()
-
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 
-x = torch.rand(projector.in_shape,
+x = torch.rand((1,1) + projector.in_shape,
                device=dev,
                dtype=torch.float32,
                requires_grad=True)
 
-y = torch.rand(projector.out_shape,
+y = torch.rand((1,1) + projector.out_shape,
                device=dev,
                dtype=torch.float32,
                requires_grad=True)
 
-fwd_layer = LinearOperatorForwardLayer.apply
-adjoint_layer = LinearOperatorAdjointLayer.apply
+fwd_layer = Linear3DOperatorForwardLayer.apply
+adjoint_layer = Linear3DOperatorAdjointLayer.apply
 
 f1 = fwd_layer(x, projector)
-f2 = projector(x.detach())
+print('forward projection (Ax) .:', f1.shape, type(f1), device(f1))
 
-assert torch.allclose(f1, f2)
 
-a1 = adjoint_layer(y, projector)
-a2 = projector.adjoint(y.detach())
+b1 = adjoint_layer(y, projector)
+print('back projection (A^T y) .:', b1.shape, type(b1), device(b1))
 
-assert torch.allclose(a1, a2)
-
-## define a dummy loss function
-#dummy_loss = (y**2).sum()
-## trigger the backpropagation
-#dummy_loss.backward()
-
-#grad_test_fwd = torch.autograd.gradcheck(fwd_layer, (x, projector),
-#                                         eps=1e-1,
-#                                         atol=1e-1,
-#                                         rtol=1e-1)
-
-#grad_test_fwd = torch.autograd.gradcheck(adjoint_layer, (y, projector),
-#                                         eps=1e-1,
-#                                         atol=1e-1,
-#                                         rtol=1e-1)
+fb1 = adjoint_layer(fwd_layer(x, projector), projector)
+print('back + forward projection (A^TAx) .:', fb1.shape, type(fb1), device(fb1))
 
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
+
+# define a dummy loss function
+dummy_loss = (fb1**2).sum()
+# trigger the backpropagation
+dummy_loss.backward()
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+print('Running forward projection layer gradient test')
+grad_test_fwd = torch.autograd.gradcheck(fwd_layer, (x, projector),
+                                         eps=1e-1,
+                                         atol=1e-4,
+                                         rtol=1e-4)
+
+print('Running adjoint projection layer gradient test')
+grad_test_fwd = torch.autograd.gradcheck(adjoint_layer, (y, projector),
+                                         eps=1e-1,
+                                         atol=1e-4,
+                                         rtol=1e-4)
