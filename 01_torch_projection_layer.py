179eeb586edeef22a98b1df86d48faa60d893c00
Georg Schramm <georg.schramm@kuleuven.be>
2023-10-17 12:24:53 +0200

    renaming

--- 01_torch_projection_layer.py
+++ 01_torch_projection_layer.py
@@ -7,7 +7,7 @@ import array_api_compat.torch as torch
 import matplotlib.pyplot as plt
 from array_api_compat import device, to_device
 
-class Linear3DOperatorForwardLayer(torch.autograd.Function):
+class LinearSingleChannel3DOperator(torch.autograd.Function):
     """
     We can implement our own custom autograd Functions by subclassing
     torch.autograd.Function and implementing the forward and backward passes
@@ -67,7 +67,7 @@ class Linear3DOperatorForwardLayer(torch.autograd.Function):
             return x, None
 
 
-class Linear3DOperatorAdjointLayer(torch.autograd.Function):
+class AdjointLinearSingleChannel3DOperator(torch.autograd.Function):
     """
     We can implement our own custom autograd Functions by subclassing
     torch.autograd.Function and implementing the forward and backward passes
@@ -179,17 +179,17 @@ y = torch.rand((1,1) + projector.out_shape,
                dtype=torch.float32,
                requires_grad=True)
 
-fwd_layer = Linear3DOperatorForwardLayer.apply
-adjoint_layer = Linear3DOperatorAdjointLayer.apply
+fwd_op_layer = LinearSingleChannel3DOperator.apply
+adjoint_op_layer = AdjointLinearSingleChannel3DOperator.apply
 
-f1 = fwd_layer(x, projector)
+f1 = fwd_op_layer(x, projector)
 print('forward projection (Ax) .:', f1.shape, type(f1), device(f1))
 
 
-b1 = adjoint_layer(y, projector)
+b1 = adjoint_op_layer(y, projector)
 print('back projection (A^T y) .:', b1.shape, type(b1), device(b1))
 
-fb1 = adjoint_layer(fwd_layer(x, projector), projector)
+fb1 = adjoint_op_layer(fwd_op_layer(x, projector), projector)
 print('back + forward projection (A^TAx) .:', fb1.shape, type(fb1), device(fb1))
 
 #----------------------------------------------------------------------------
@@ -208,13 +208,13 @@ dummy_loss.backward()
 #----------------------------------------------------------------------------
 
 print('Running forward projection layer gradient test')
-grad_test_fwd = torch.autograd.gradcheck(fwd_layer, (x, projector),
+grad_test_fwd = torch.autograd.gradcheck(fwd_op_layer, (x, projector),
                                          eps=1e-1,
                                          atol=1e-4,
                                          rtol=1e-4)
 
 print('Running adjoint projection layer gradient test')
-grad_test_fwd = torch.autograd.gradcheck(adjoint_layer, (y, projector),
+grad_test_fwd = torch.autograd.gradcheck(adjoint_op_layer, (y, projector),
                                          eps=1e-1,
                                          atol=1e-4,
                                          rtol=1e-4)
