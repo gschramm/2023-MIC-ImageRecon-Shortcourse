e506f7510cc5467c171e0b6f7baf97bbd0e24641
Georg Schramm <georg.schramm@kuleuven.be>
2023-10-27 12:44:01 +0200

    improve command line parsing

--- zz_osem_varnet.py
+++ zz_osem_varnet.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-#""" TODO: - non-random subset order
-#          - depth independent from num subsets
-#          - batch size validation
-#          - unet resnet style - for better pre-training
-#"""        
-
+import argparse
+import json
+from datetime import datetime
 import utils
 import parallelproj
 import array_api_compat.torch as torch
@@ -83,28 +80,66 @@ class SimpleOSEMVarNet(torch.nn.Module):
 
         return x
 
+class PostReconNet(torch.nn.Module):
+    """dummy cascaded model that includes layers combining projections and convolutions"""
+
+    def __init__(self,
+                 neural_net: torch.nn.Module) -> None:
+        super().__init__()
+        self._neural_net = neural_net
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # fusion of EM update and neural net update with trainable weight
+        # we use an ReLU activation to ensure that the output of each block is non-negative
+        return torch.nn.ReLU()(x + self._neural_net(x))
+
+
 #-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser(description='OSEM-VARNet reconstruction')
+parser.add_argument('--num_datasets', type=int, default=60)
+parser.add_argument('--num_training', type=int, default=40)
+parser.add_argument('--num_validation', type=int, default=20)
+parser.add_argument('--num_subsets', type=int, default=4)
+parser.add_argument('--depth', type=int, default=4)
+parser.add_argument('--num_epochs', type=int, default=500)
+parser.add_argument('--num_epochs_post', type=int, default=200)
+parser.add_argument('--batch_size', type=int, default=10)
+parser.add_argument('--num_features', type=int, default=16)
+parser.add_argument('--num_rings', type=int, default=4)
+parser.add_argument('--radial_trim', type=int, default=181)
+parser.add_argument('--voxel_size', nargs='+', type=float, default = [2.5, 2.5, 2.66])
+
+args = parser.parse_args()
+
+num_datasets = args.num_datasets
+num_training = args.num_training
+num_validation = args.num_validation
+num_subsets = args.num_subsets
+depth = args.depth
+num_epochs = args.num_epochs
+num_epochs_post = args.num_epochs_post
+batch_size = args.batch_size
+num_features = args.num_features
+num_rings = args.num_rings
+radial_trim = args.radial_trim
+voxel_size = tuple(args.voxel_size)
 
 # device variable (cpu or cuda) that determines whether calculations
 # are performed on the cpu or cuda gpu
-
 if parallelproj.cuda_present:
     dev = 'cuda'
 else:
     dev = 'cpu'
 
-num_datasets = 60
-num_training = 40
-num_validation = 20
-num_subsets = 1
-depth = 8
-num_epochs = 500
-batch_size = 3
-num_features = 16
+output_dir = Path('run_osem_varnet') / f'{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+output_dir.mkdir(exist_ok=True, parents=True)
 
+with open(output_dir / 'input_csf.json', 'w') as f:
+    json.dump(vars(args), f)
 
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
@@ -114,8 +149,6 @@ num_features = 16
 
 # setup a line of response descriptor that describes the LOR start / endpoints of
 # a "narrow" clinical PET scanner with 9 rings
-num_rings = 4
-radial_trim = 181
 lor_descriptor = utils.DemoPETScannerLORDescriptor(torch,
                                                    dev,
                                                    num_rings=num_rings,
@@ -130,12 +163,8 @@ axial_fov_mm = float(lor_descriptor.scanner.num_rings *
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 
-
 # image properties
 ids = tuple([i for i in range(num_datasets)])
-batch_size = len(ids)
-voxel_size = (2.5, 2.5, 2.66)
-#voxel_size = (2.66, 2.66, 2.66)
 
 emission_image_database, attenuation_image_database = load_brain_image_batch(
     ids,
@@ -180,7 +209,7 @@ osem_update_modules = [
     EMUpdateModule(projector) for projector in subset_projectors.operators
 ]
 
-osem_database = torch.ones((batch_size, 1) + subset_projectors.in_shape,
+osem_database = torch.ones((num_datasets, 1) + subset_projectors.in_shape,
                            device=dev,
                            dtype=torch.float32)
 
@@ -207,22 +236,24 @@ for i in range(num_osem_iter):
 print('\npostrecon unet training\n')
 
 
-post_recon_unet = Unet3D(num_features=num_features).to(dev)
+post_recon_unet = PostReconNet(Unet3D(num_features=num_features).to(dev))
 post_recon_unet.train()
 
 loss_fn_post = torch.nn.MSELoss()
 optimizer_post = torch.optim.Adam(post_recon_unet.parameters(), lr=1e-3)
 
-loss_arr_post = torch.zeros(num_epochs)
+loss_arr_post = torch.zeros(num_epochs_post)
 
-for epoch in range(num_epochs):
+min_val_loss_post = float('inf')
+
+for epoch in range(num_epochs_post):
     batch_inds = torch.split(torch.randperm(num_training), batch_size)
 
     for ib, batch_ind in enumerate(batch_inds):
         x_fwd_post = post_recon_unet(osem_database[batch_ind, ...])
         loss_post = loss_fn_post(x_fwd_post, emission_image_database[batch_ind, ...])
 
-        print(f'{(epoch+1):03}/{num_epochs:03} {(ib+1):03} {loss_post:.2E}',
+        print(f'{(epoch+1):03}/{num_epochs_post:03} {(ib+1):03} {loss_post:.2E}',
               end='\r')
 
         # Backpropagation
@@ -236,77 +267,90 @@ for epoch in range(num_epochs):
         post_recon_unet.eval()
         x_fwd_post = post_recon_unet(osem_database[num_training:(num_training+num_validation), ...])
         val_loss_post = loss_fn_post(x_fwd_post, emission_image_database[num_training:(num_training+num_validation), ...])
-        print(f'{(epoch+1):03}/{num_epochs:03} val_loss {val_loss_post:.2E}')
+        print(f'{(epoch+1):03}/{num_epochs_post:03} val_loss {val_loss_post:.2E}')
         post_recon_unet.train()
-   
 
-#--------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------
-# model training
-#--------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------
+        if val_loss_post < min_val_loss_post:
+            min_val_loss_post = val_loss_post
+            torch.save(post_recon_unet._neural_net.state_dict(), output_dir / 'post_recon_model_best_state.pt')
 
-print('\nvarnet training\n')
+torch.save(post_recon_unet._neural_net.state_dict(), output_dir / 'post_recon_model_last_state.pt')
 
-unet = Unet3D(num_features=num_features).to(dev)
-osem_var_net = SimpleOSEMVarNet(osem_update_modules, unet, depth, dev)
-osem_var_net.train()
-
-loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(osem_var_net.parameters(), lr=1e-3)
-
-loss_arr = torch.zeros(num_epochs)
-
-for epoch in range(num_epochs):
-    batch_inds = torch.split(torch.randperm(num_training), batch_size)
-
-    for ib, batch_ind in enumerate(batch_inds):
-        x_fwd = osem_var_net(osem_database[batch_ind, ...],
-                             emission_data_database[:, batch_ind, ...],
-                             correction_database[:, batch_ind, ...],
-                             contamination_database[:, batch_ind, ...],
-                             adjoint_ones_database[:, batch_ind, ...])
-
-        loss = loss_fn(x_fwd, emission_image_database[batch_ind, ...])
-
-        print(f'{(epoch+1):03}/{num_epochs:03} {(ib+1):03} {loss:.2E}',
-              end='\r')
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-    loss_arr[epoch] = loss
-
-    if (epoch+1) % 20 == 0:
-        osem_var_net.eval()
-        x_fwd = osem_var_net(osem_database[num_training:(num_training+num_validation), ...],
-                             emission_data_database[:,num_training:(num_training+num_validation) , ...],
-                             correction_database[:,num_training:(num_training+num_validation) , ...],
-                             contamination_database[:,num_training:(num_training+num_validation) , ...],
-                             adjoint_ones_database[:,num_training:(num_training+num_validation) , ...])
-        val_loss = loss_fn(x_fwd, emission_image_database[num_training:(num_training+num_validation), ...])
-        print(f'{(epoch+1):03}/{num_epochs:03} val_loss {val_loss:.2E}')
-        osem_var_net.train()
- 
-output_dir = tempfile.TemporaryDirectory(dir = '.', prefix = 'run_osem_varnet_')
-output_path = Path(output_dir.name) / 'model_state.pt'
-torch.save(osem_var_net.state_dict(), str(output_path))
-print(f'save model to {str(output_path)}')
+del post_recon_unet
+  
 
 ##--------------------------------------------------------------------------------
 ##--------------------------------------------------------------------------------
-## model evaluation
+## model training
 ##--------------------------------------------------------------------------------
 ##--------------------------------------------------------------------------------
 #
-#osem_var_net.load_state_dict(torch.load('osem_varnet_4ss.pt',
-#                                        map_location=dev))
-#osem_var_net.eval()
+#print('\nvarnet training\n')
 #
-#x_fwd = osem_var_net(osem_database[:, ...], emission_data_database[:, :, ...],
-#                     correction_database[:, :, ...],
-#                     contamination_database[:, :,
-#                                            ...], adjoint_ones_database[:, :,
+#unet = Unet3D(num_features=num_features).to(dev)
+#unet.load_state_dict(torch.load(str(post_output_path)))
 #
+#osem_var_net = SimpleOSEMVarNet(osem_update_modules, unet, depth, dev)
+#osem_var_net.train()
+#
+#loss_fn = torch.nn.MSELoss()
+#optimizer = torch.optim.Adam(osem_var_net.parameters(), lr=1e-3)
+#
+#loss_arr = torch.zeros(num_epochs)
+#
+#for epoch in range(num_epochs):
+#    batch_inds = torch.split(torch.randperm(num_training), batch_size)
+#
+#    for ib, batch_ind in enumerate(batch_inds):
+#        x_fwd = osem_var_net(osem_database[batch_ind, ...],
+#                             emission_data_database[:, batch_ind, ...],
+#                             correction_database[:, batch_ind, ...],
+#                             contamination_database[:, batch_ind, ...],
+#                             adjoint_ones_database[:, batch_ind, ...])
+#
+#        loss = loss_fn(x_fwd, emission_image_database[batch_ind, ...])
+#
+#        print(f'{(epoch+1):03}/{num_epochs:03} {(ib+1):03} {loss:.2E}',
+#              end='\r')
+#
+#        # Backpropagation
+#        loss.backward()
+#        optimizer.step()
+#        optimizer.zero_grad()
+#
+#    loss_arr[epoch] = loss
+#
+#    if (epoch+1) % 20 == 0:
+#        osem_var_net.eval()
+#        val_loss = 0
+#        for iv in range(num_training, (num_training+num_validation)):
+#            x_fwd = osem_var_net(osem_database[iv:(iv+1), ...],
+#                                 emission_data_database[:,iv:(iv+1) , ...],
+#                                 correction_database[:,iv:(iv+1) , ...],
+#                                 contamination_database[:,iv:(iv+1) , ...],
+#                                 adjoint_ones_database[:,iv:(iv+1) , ...])
+#            val_loss += float(loss_fn(x_fwd, emission_image_database[iv:(iv+1), ...]))
+#
+#        val_loss /= num_validation
+#        print(f'{(epoch+1):03}/{num_epochs:03} val_loss {val_loss:.2E}')
+#        osem_var_net.train()
+# 
+#output_path = Path(output_dir.name) / 'model_state.pt'
+#torch.save(osem_var_net.state_dict(), str(output_path))
+#print(f'save model to {str(output_path)}')
+#
+###--------------------------------------------------------------------------------
+###--------------------------------------------------------------------------------
+### model evaluation
+###--------------------------------------------------------------------------------
+###--------------------------------------------------------------------------------
+##
+##osem_var_net.load_state_dict(torch.load('osem_varnet_4ss.pt',
+##                                        map_location=dev))
+##osem_var_net.eval()
+##
+##x_fwd = osem_var_net(osem_database[:, ...], emission_data_database[:, :, ...],
+##                     correction_database[:, :, ...],
+##                     contamination_database[:, :,
+##                                            ...], adjoint_ones_database[:, :,
+##
